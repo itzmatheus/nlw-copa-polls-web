ee33930cbde31479f1ca6cd01f1ea64c3c2109ab
Matheus Leite <matheusjoselfm@icloud.com>
2022-11-02 23:25:08 -0300

    feat: add submit create pool

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,37 @@ import logoImg from '../assets/logo.svg'
 import usersAvatarExampleImg from '../assets/users-avatar-example.png'
 import iconCheckImg from '../assets/icon-check.svg'
 import { api } from '../lib/axios'
+import { FormEvent, useState } from 'react'
 
 interface HomeProps {
-  poolCount: Number
-  guessCount: Number
-  userCount: Number
+  poolCount: number
+  guessCount: number
+  userCount: number
 }
 
 
 export default function Home(props: HomeProps) {
+
+  const [poolTitle, setPoolTitle] = useState('')
+
+  async function createPool(event: FormEvent) {
+    event.preventDefault()
+
+    try {
+      const response = await api.post('/pools', {
+        title: poolTitle,
+      })
+
+      const { code } = response.data
+      alert(`Bolão criado com sucesso, utilize o código: ${code} para compartilhar com seus amigos!`)
+      setPoolTitle('')
+    } catch (err) {
+      console.log(err);
+      alert('Falha ao criar o bolão, tente novamente!')
+    }
+
+  }
+
   return (
     <div className='max-w-[1124px] h-screen mx-auto grid gap-28 grid-cols-2 items-center'> 
       <main>
@@ -30,11 +52,13 @@ export default function Home(props: HomeProps) {
           </strong>
         </div>
  
-        <form className='mt-10 flex gap-2'>
+        <form onSubmit={createPool} className='mt-10 flex gap-2'>
           <input
-            className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm'
+            className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100'
             type="text"
             placeholder='Qual nome do seu bolão?'
+            onChange={event => setPoolTitle(event.target.value)}
+            value={poolTitle}
           />
           <button
             className='bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700'
